9290cc6889ccbc00dcd4fd75b65bad3f24a4dff8
pedroandreou <pandreou01@gmail.com>
2022-07-23 00:20:59 +0300

    Optimize code

--- src/check_string.py
+++ src/check_string.py
@@ -2,22 +2,21 @@ import re
 
 
 def check_string(file_read, text, pattern=False):
+    
+    file_lst = file_read.split(" ")
 
     if pattern == False:        
-        try:
-            # check if the text is the first or last word of the file
-            if file_read.split(" ",1)[0] == text or file_read.split(" ")[-1] == text:
-                return True
-        except IndexError:
-            return False
-        
-        # check if the text is part of the file
-        if not file_read.find(' ' + text + ' ') == -1:
-                return True
-                
-    else:
         # check if the text is the first or last word of the file
-        if re.search('^' + text + '$', file_read.split(" ",1)[0]) is not None or re.search('^' + text + '$', file_read.split(" ")[-1]) is not None:
+        if file_lst[0] == text or file_lst[-1] == text:
+            return True
+        # check if the text is part of the file
+        elif not file_read.find(' ' + text + ' ') == -1:
+                return True
+    else:
+        new_text = '^' + text + '$'
+
+        # check if the text is the first or last word of the file
+        if re.search(new_text, file_lst[0]) is not None or re.search(new_text, file_lst[-1]) is not None:
             return True
         # check if the text is part of the file
         elif re.search(' ' + text + ' ', file_read) is not None:
